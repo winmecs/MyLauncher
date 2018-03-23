8e954c04e73a21065da145a95f7408905421d51b
winme <gaochengsiwork@gmail.com>
2018-03-23 12:07:02 +0800

    Default show calculator in launcher

--- app/src/main/java/com/example/winme/mylauncher/MyLauncher.java
+++ app/src/main/java/com/example/winme/mylauncher/MyLauncher.java
@@ -1,7 +1,15 @@
 package com.example.winme.mylauncher;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyLauncher extends AppCompatActivity {
 
@@ -9,5 +17,29 @@ public class MyLauncher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_launcher);
+
+        ImageView chromeIcon = (ImageView) findViewById(R.id.chromeButton);
+        chromeIcon.setImageDrawable(getActivityIcon(this, "com.android.calculator2", "com.android.calculator2.Calculator"));
     }
+
+    public static Drawable getActivityIcon(Context context, String packageName, String activityName) {
+        PackageManager pm = context.getPackageManager();
+        Intent intent = new Intent();
+        intent.setComponent(new ComponentName(packageName, activityName));
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, 0);
+
+        return resolveInfo.loadIcon(pm);
+    }
+
+    public void onChromeButtonClick(View v) {
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.android.calculator2");
+        startActivity(launchIntent);
+    }
+
+//   handle the back button
+    @Override
+    public void onBackPressed(){
+
+    }
+
 }
