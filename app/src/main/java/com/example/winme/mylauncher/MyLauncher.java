36c0830a078dfd101b45a21b2d25eb787ee00b91
winme <gaochengsiwork@gmail.com>
2018-03-29 15:17:50 +0800

    Server can return html

--- app/src/main/java/com/example/winme/mylauncher/MyLauncher.java
+++ app/src/main/java/com/example/winme/mylauncher/MyLauncher.java
@@ -5,13 +5,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.content.res.AssetManager;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MyLauncher extends AppCompatActivity {
 
@@ -22,8 +29,14 @@ public class MyLauncher extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_launcher);
 
+        File index = new File(getFilesDir(),"index.html");
+        // First time to start app
+        if(!index.exists())
+            copyAssets();
+
+
         try {
-            server = new MyServer();
+            server = new MyServer(this);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -50,5 +63,51 @@ public class MyLauncher extends AppCompatActivity {
     @Override
     public void onBackPressed(){
 
+    }
+
+
+    private void copyAssets() {
+        AssetManager assetManager = getAssets();
+        String[] files = null;
+        try {
+            files = assetManager.list("");
+        } catch (IOException e) {
+            Log.e("tag", "Failed to get asset file list.", e);
+        }
+        if (files != null) for (String filename : files) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                in = assetManager.open(filename);
+                File outFile = new File(getFilesDir(), filename);
+                out = new FileOutputStream(outFile);
+                copyFile(in, out);
+            } catch(IOException e) {
+                Log.e("tag", "Failed to copy asset file: " + filename, e);
+            }
+            finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+                if (out != null) {
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        // NOOP
+                    }
+                }
+            }
+        }
+    }
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while((read = in.read(buffer)) != -1){
+            out.write(buffer, 0, read);
+        }
     }
 }
--- app/src/main/java/com/example/winme/mylauncher/MyServer.java
+++ app/src/main/java/com/example/winme/mylauncher/MyServer.java
@@ -5,24 +5,48 @@ package com.example.winme.mylauncher;
  */
 
 
+
+import android.content.Context;
+import android.os.Environment;
+
 import org.nanohttpd.protocols.http.IHTTPSession;
 import org.nanohttpd.protocols.http.NanoHTTPD;
+import org.nanohttpd.protocols.http.request.Method;
 import org.nanohttpd.protocols.http.response.Response;
+import org.nanohttpd.protocols.http.response.Status;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class MyServer extends NanoHTTPD {
     private final static int PORT = 8080;
-    public MyServer() throws IOException {
+    private Context context;
+    public MyServer(Context Lcontex) throws IOException {
         super(PORT);
+        context = Lcontex;
         start();
-        System.out.println("\nRunning! Point your browers to http://localhost:8080/ \\n");
     }
 
     @Override
     public Response serve(IHTTPSession session) {
-        String msg = "<html><body><h1>Hello server</h1>\n";
-        msg += "<p>We serve " + session.getUri() + " !</p>";
-        return Response.newFixedLengthResponse( msg + "</body></html>\n" );
+        Method method = session.getMethod();
+        if(Method.GET.equals(method)){
+            String url = session.getUri();
+            if(url.equals("/")){
+                FileInputStream index =null;
+                try {
+                    index = new FileInputStream(context.getFilesDir() + "/index.html");
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                return Response.newChunkedResponse(Status.OK, "text/html",index);
+            }
+        }
+        else if (Method.POST.equals(method)){
+            String msg = "<html><body><h1>get post</h1>\n";
+            return Response.newFixedLengthResponse(msg + "</body></html>\n");
+        }
+        return Response.newFixedLengthResponse("not found");
     }
 }
